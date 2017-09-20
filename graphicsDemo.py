dc0545fa250590a7f4cfb7ec80db8c52df4d5aa7
maxmyth01 <maxmyth01@gmail.com>
2017-09-20 13:36:11 -0400

    Updated from Brython Server: 9/20/2017 1:36:10 PM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -4,4 +4,16 @@
 
 from ggame import *
 
+red = Color(0xFF0000,1)
+
+green = Color(0x00FF00,1)
+
+blue = Color(0x0000FF,1)
+
+black = Color(0xFFFFFF,1)
+
+blackoutline = LineStyle(1,black) # (pixels,colors)
+
+
+
 App().run()
