29f857a265908b1547588fdc2481faf18dbc1857
maxmyth01 <maxmyth01@gmail.com>
2017-09-20 13:50:13 -0400

    Updated from Brython Server: 9/20/2017 1:50:12 PM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -13,18 +13,18 @@ blue = Color(0x0000FF,1)
 black = Color(0x000000,1)
 
 blackOutline = LineStyle(5,black) # (pixels,colors)
-
 redRectangle = RectangleAsset(200,100,blackOutline,red) # (width, height, outline, fill)
+blueCircle = CircleAsset(50,blackOutline,blue) # radius, linestyle,color
+greenEllipse = EllipseAsset(100,50,blackOutline,green) # major axis, minor axis, linestyle, color
+blackLine = LineAsset(50,160,blackOutline) # x endpt, y endpt, linestyle
+redTriangle = PolygonAsset([(0,0), (120,180), (60,300), blackOutline, red])
 
-blueCircle = CircleAsset(50,blackOutline,blue)
-
-greenEllipse = EllipseAsset(100,50,blackOutline,green)
 
 Sprite(redRectangle)
-
 Sprite(blueCircle,(50,50))
-
 Sprite(greenEllipse,(200,400))
+sprite(blackLine)
+sprite(redTriangle)
 
 
 App().run()
