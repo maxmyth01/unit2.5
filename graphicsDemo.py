1419705f80f5bc8c955c0f333b70f91a7d06cd2e
maxmyth01 <maxmyth01@gmail.com>
2017-09-20 13:54:31 -0400

    Updated from Brython Server: 9/20/2017 1:54:30 PM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -5,11 +5,8 @@
 from ggame import *
 
 red = Color(0xFF0000,1)
-
 green = Color(0x00FF00,1)
-
 blue = Color(0x0000FF,1)
-
 black = Color(0x000000,1)
 
 blackOutline = LineStyle(5,black) # (pixels,colors)
@@ -17,7 +14,8 @@ redRectangle = RectangleAsset(200,100,blackOutline,red) # (width, height, outlin
 blueCircle = CircleAsset(50,blackOutline,blue) # radius, linestyle,color
 greenEllipse = EllipseAsset(100,50,blackOutline,green) # major axis, minor axis, linestyle, color
 blackLine = LineAsset(50,160,blackOutline) # x endpt, y endpt, linestyle
-redTriangle = PolygonAsset([(0,0), (120,180), (60,300), blackOutline, red])
+redTriangle = PolygonAsset([(0,0), (120,180), (60,300)], blackOutline, red)
+text = TextAsset('Max',fill=green,style='bold 40pt Times')
 
 
 Sprite(redRectangle)
@@ -25,6 +23,7 @@ Sprite(blueCircle,(50,50))
 Sprite(greenEllipse,(200,400))
 sprite(blackLine)
 sprite(redTriangle)
+sprite(text,(300,200))
 
 
 App().run()
