f35773a5390e35655ab57b2032985981842fd111
maxmyth01 <maxmyth01@gmail.com>
2017-09-22 14:52:17 -0400

    Updated from Brython Server: 9/22/2017 2:52:16 PM

--- name.py
+++ name.py
@@ -3,14 +3,18 @@
 #name.py -- prints name in middle and choose a backround color
 
 from ggame import *
+
+name = input('Enter your Name')
 color = input('Enter a RGB color code')
 color = Color(color,1)
 black = Color(0x000000,1)
 
 blackOutline = LineStyle(0.5,black)
 redRectangle = RectangleAsset(1050,700,blackOutline,color)
+text = TextAsset(name,fill=black,style='bold 70pt Times')
 
 Sprite(redRectangle)
+Sprite(text,(475,200))
 
 App().run()
 
