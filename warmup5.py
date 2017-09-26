f91bf86297553f784623d9e5500fd94d49f6a961
maxmyth01 <maxmyth01@gmail.com>
2017-09-26 14:15:07 -0400

    Updated from Brython Server: 9/26/2017 2:15:06 PM

--- warmup5.py
+++ warmup5.py
@@ -1,1 +1,19 @@
+#Max Low
+#9-26-17
+#warmup5.py -- yellow dimond with name inside
 
+from ggame import *
+
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+yellow =Color(0xFFFF00,1)
+
+blackOutline = LineStyle(5,black) # (pixels,colors)
+yellowDiamond = PolygonAsset([(0,0), (-150,300), (0,600), (150,300)], blackOutline, yellow)
+text = TextAsset('Max',fill=blue,style='bold 40pt Times')
+
+Sprite(yellowDiamond,(300,200))
+Sprite(text,(300,200))
+
+
+App().run()
