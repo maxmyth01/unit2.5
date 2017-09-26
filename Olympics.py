9e7ec423396dd6760934c02141d20ef6fc46c635
maxmyth01 <maxmyth01@gmail.com>
2017-09-26 14:32:36 -0400

    Updated from Brython Server: 9/26/2017 2:32:34 PM

--- Olympics.py
+++ Olympics.py
@@ -1,1 +1,20 @@
+#Max Low
+#9-26-17
+#olympics.py -- print olympic rings using colored and white circles
+
+from ggame import *
+
+blue = Color(0x0000FF,1)
+black = Color(0x000000,1)
+yellow =Color(0xFFFF00,1)
+
+blackOutline = LineStyle(5,black) # (pixels,colors)
+yellowDiamond = PolygonAsset([(0,0), (-75,150), (0,300), (75,150)], blackOutline, yellow)
+text = TextAsset('Max',fill=blue,style='bold 40pt Times')
+
+Sprite(yellowDiamond,(350,100))
+Sprite(text,(300,200))
+
+
+App().run()
 
