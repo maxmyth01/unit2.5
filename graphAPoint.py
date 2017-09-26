60541b0ddf851069a3ecfd5152c69ea1881ca9f2
maxmyth01 <maxmyth01@gmail.com>
2017-09-26 14:53:54 -0400

    Updated from Brython Server: 9/26/2017 2:53:53 PM

--- graphAPoint.py
+++ graphAPoint.py
@@ -1,1 +1,20 @@
+#Max Low
+#9-26-17
+#graphAPoint.py -- graphs a point between -10,10
 
+from ggame import *
+
+black = Color(0x000000,1)
+
+blackOutline = LineStyle(5,black) # (pixels,colors)
+blackLine = LineAsset(50,160,blackOutline) # x endpt, y endpt, linestyle
+xaxis = LineAsset(50,160,blackOutline)
+yaxis = LineAsset(50,160,blackOutline)
+
+Sprite(xaxis,(50,50))
+Sprite(yaxis,(100,100))
+Sprite(blackLine,(150,50))
+
+
+
+App().run()
